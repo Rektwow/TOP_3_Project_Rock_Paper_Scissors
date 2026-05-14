6026beca79ed05378235b529bf906afe2e8cde81
YassineBM <sae.yessin@outlook.com>
2026-05-14 22:43:28 +0200

    created score function to track winner

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 // this function is the input the user gives
 function getHumanChoice() {
   let input = prompt("Choose: Rock, Paper or Scissors");
@@ -24,18 +26,52 @@ function getHumanChoice() {
     // Asks user for correct input again until it's correct
   }
 }
-console.log("You chose: " + getHumanChoice());
+//console.log("You chose: " + getHumanChoice());
 
+// this function is where the computer chooses randomly
 function getComputerChoice() {
   let choice;
+  // returns number between 0 and 98 ( I chose that to it's always 33% to hit)
   let randomNumber = Math.floor(Math.random() * 99);
+
   if (randomNumber < 33) {
-    Choice = "Rock";
+    choice = "Rock";
   } else if (randomNumber < 66) {
-    Choice = "Paper";
+    choice = "Paper";
   } else {
-    Choice = "Scissors";
+    choice = "Scissors";
   }
-  console.log("Computer chose: " + Choice);
+  //console.log("Computer chose: " + choice);
+  return choice;
 }
-getComputerChoice();
+//getComputerChoice();
+
+let human = getHumanChoice();
+console.log("You chose: " + human);
+let computer = getComputerChoice();
+console.log("computer chose: " + computer);
+
+// this function will keep track of the score
+function score() {
+  if (human === "Rock" && computer === "Paper") {
+    computerScore++;
+    console.log("whatever");
+  } else if (human === computer) {
+    return null;
+  } else if (human === "Paper" && computer === "Scissors") {
+    computerScore++;
+  } else if (human === "Scissors" && computer === "Rock") {
+    computerScore++;
+  } else if (human === "Rock" && computer === "Scissors") {
+    humanScore++;
+  } else if (human === "Paper" && computer === "Rock") {
+    humanScore++;
+  } else if (human === "Scissors" && computer === "Paper") {
+    humanScore++;
+  } else {
+    return null;
+  }
+}
+score(human, computer);
+console.log("you " + humanScore);
+console.log("pc " + computerScore);
